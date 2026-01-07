45d0245d65201da02783b065f9f2459fc3656285
Kiel Cruey <kscruey@gmail.com>
2026-01-06 18:40:18 -0700

    init commit

---
+++ creating-targets/maths/maths.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+#include "maths.hpp"
+
+namespace math {
+	double add(double a, double b){
+		return a + b;
+	}
+
+	double subtract(double a, double b){
+		return a - b;
+	}
+
+	double multiply(double a, double b){
+		return a * b;
+	}
+
+	double divide(double a, double b){
+		if(b == 0){
+			std::cerr << "Error: Division by zero." << std::endl;
+			return 0; // or handle error as appropriate
+		}
+
+		return a / b;
+	}
+}
---
+++ creating-targets/maths/maths.hpp
@@ -0,0 +1,6 @@
+namespace math {
+	double add(double a, double b);
+	double subtract(double a, double b);
+	double multiply(double a, double b);
+	double divide(double a, double b);
+}
