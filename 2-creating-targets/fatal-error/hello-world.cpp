17d0e5cf0f0e7e1c2f7b24508d2c54e7727737dd
Kiel Cruey <kscruey@gmail.com>
2026-01-12 11:40:49 -0700

    changed file structure for kcruey.com website

---
+++ 2-creating-targets/fatal-error/hello-world.cpp
@@ -0,0 +1,8 @@
+#include <cstdio>
+#include <cstdlib> // EXIT_SUCCESS
+
+int main()
+{
+  std::printf("Hello World\n");
+  return EXIT_SUCCESS;
+}
---
+++ 2-creating-targets/project-example/CMakeGraphVizOptions.cmake
@@ -0,0 +1,5 @@
+message("")
+message("GRAPHVIZ option(s) confirmed")
+message("")
+
+set(GRAPHVIZ_CUSTOM_TARGET TRUE)
